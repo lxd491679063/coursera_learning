f67bffc4fdd396aae43d96b546171e091a4d662c
lxd <lxd491679063@163.com>
2021-06-22 18:13:44 +0800

    php_test

---
+++ test_php/index.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+<meta http-equiv="pragma" content="no-cache"/>
+<meta http-equiv="cache-control" content="no-cache,must-revalidate"/>
+<meta http-equiv="expires" content="0"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>热血三国复刻版后台</title>
+
+</head>
+<body>
+	<form action="login_gm.php" method="post">
+	账号：<input type="text" name="passport" value=""/><br/>
+	<input type="submit" name="submit" value="登陆" style="height:100px;width:240px" />
+	</form>
+</body>
+</html>
